305bcc75c2f438cc59e951261efd734f314927bc
luj00 <145015534+luj00@users.noreply.github.com>
2024-02-20 15:38:10 +0300

    Created order schemas in the customer model

--- server/models/customer.js
+++ server/models/customer.js
@@ -1,2 +1,35 @@
-//add basket schema here
-//add order schema here
+const mongoose = require('mongoose');
+
+// Define the order schema
+const orderSchema = new mongoose.Schema({
+    orderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Customer',
+      required: true
+    },
+    restaurantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Restaurant',
+      required: true
+    },
+    itemId: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Item'
+    }],
+    status: {
+      type: String,
+      enum: ['Pending', 'In Progress', 'Completed'],
+      default: 'Pending'
+    },
+    creationDate: {
+      type: Date,
+      default: Date.now
+    },
+    totalBill: {
+      type: mongoose.Schema.Types.Decimal128
+    }
+});
