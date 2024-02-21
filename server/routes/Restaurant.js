0634e4ed94d51eab1a5002a532889f3e0c846501
Rinad Abu Qauod <rinadaq98@gmail.com>
2024-02-21 22:37:01 +0300

    First part done

--- server/routes/Restaurant.js
+++ server/routes/Restaurant.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const retaurantController = require("../controllers/Restaurant");
+const restaurantController = require("../controllers/Restaurant");
+
+// router.post('/SignIn', restaurantController.restaurantSignIn);
+// router.post('/SignUp', restaurantController.restaurantSignUp);
+// router.post('/addMenuItem', restaurantController.addMenuItem);
+// router.put('/updateItem/:itemId', restaurantController.updateMenuItem);
+// router.delete('/removeItem/:itemId', restaurantController.removeMenuItem);
+// router.get('/restaurantInfo/:restaurantId', restaurantController.getRestaurantInfo);
+// router.put('/restaurantInfo/update/:restaurantId', restaurantController.updateRestaurantInfo);
 
 module.exports = router;
