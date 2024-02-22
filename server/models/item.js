755b42f1a0bfb1b093069a8a354833c58f08aa73
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2024-02-22 22:13:12 +0300

    changed item model name

--- server/models/item.js
+++ server/models/item.js
@@ -1,39 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
-
   //itemId:{
-    //    type: mongoose.Schema.Types.ObjectId,
-    //    required: true,
-    //   unique: true
-    // },
-    restaurantId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Restaurant',
-        required: true
-    },
-    name:{
-        type: String,
-        required: true
-    },
-    description:{
-        type: String,
-        required: true
-    },
-    image:{
-        type: String
-    },
-    price:{
-        type: mongoose.Schema.Types.Decimal128,
-        required: true
-    },
-    category:{
-        type: String,
-        enum: ['Desserts', 'Breakfast', 'Traditional', 'Beverages', 'Snacks'], 
-        default: 'uncategorized'
-    }
+  //    type: mongoose.Schema.Types.ObjectId,
+  //    required: true,
+  //   unique: true
+  // },
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  price: {
+    type: mongoose.Schema.Types.Decimal128,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ["Desserts", "Breakfast", "Traditional", "Beverages", "Snacks"],
+    default: "uncategorized",
+  },
 });
 
-const item = mongoose.model('item', itemSchema);
+const item = mongoose.model("Item", itemSchema);
 
 module.exports = item;
