d9edd4206dc677b31583ca6c652a8c3c36b5a82a
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2024-02-23 20:04:52 +0300

    implementd the sign up without testing

--- server/server/controllers/customer.js
+++ server/server/controllers/customer.js
@@ -2,7 +2,27 @@ const moongose = require("mongoose");
 const customerModel = require("../models/customer");
 const itemModel = require("../models/item");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-async function signup(req, res) {}
+async function signup(req, res) {
+  const { password, password2 } = req.body;
+  try {
+    if (password !== password2) throw new Error("passwords doesn't match");
+    const hashedPassword = await bcrypt.hash(password, 10);
+    //remove the password confirmation from the request body
+    delete req.body.password2;
+    //replace the password with hashed password
+    req.body.password = hashedPassword;
+    const newCustomer = await customerModel.create(req.body);
+    const token = jwt.sign(
+      { userId: newCustomer._id },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(422).json({ message: err.message });
+  }
+}
 
-module.exports = {};
+module.exports = { signup };
