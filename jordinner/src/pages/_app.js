f27a684b77620f1ccaf798eadf285cb95bc223d1
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2024-03-13 16:52:39 +0300

    small tweak

--- jordinner/src/pages/_app.js
+++ jordinner/src/pages/_app.js
@@ -1,10 +1,11 @@
 import "@/styles/globals.css";
 import NavbarBefore from "@/components/navbar-before";
-
+import CustomerNav from "./customer/customerNav";
 export default function App({ Component, pageProps }) {
   return (
     <>
       {/* <NavbarBefore /> */}
+      <CustomerNav />
       <Component {...pageProps} />
     </>
   );
