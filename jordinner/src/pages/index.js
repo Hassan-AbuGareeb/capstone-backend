ea524148c1a487f6068951e2ec41626e30c19e9a
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2024-03-14 14:04:59 +0300

    fixed restaurant issues in home page

--- jordinner/src/pages/index.js
+++ jordinner/src/pages/index.js
@@ -1,11 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import foodImage from "../../public/big-food-image.jpg";
+// import foodImage from "../../public/big-food-image.jpg";
 
 export default function Home() {
   const [dishes, setDishes] = useState([]);
-  const [restaurants, setrestaurants] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
 
   async function getDishes() {
     const dishesResponse = await fetch("http://localhost:3001/restaurants/get");
@@ -19,7 +19,7 @@ export default function Home() {
       "http://localhost:3001/restaurants/"
     );
     const restaurantsData = await restaurantsResponse.json();
-    setDishes([...restaurantsData]);
+    setRestaurants([...restaurantsData]);
   }
 
   useEffect(() => {
@@ -32,36 +32,44 @@ export default function Home() {
       {/* dish image */}
       <p>{dish.name}</p>
       <p>{dish.description}</p> {/* Fixing the typo here */}
-      <p>{dish.price.$numberDecimal}</p>
+      {/* <p>{dish.price.$numberDecimal}</p> */}
     </div>
   ));
 
-  // const restaurantsCards = dishes.map((dish) => (
-  //   <div key={dish.name} className="flex flex-col border-2 border-black gap-4">
-  //     <p>{dish.name}</p>
-  //     <p>{dish.description}</p> {/* Fixing the typo here */}
-  //     <p>{dish.price.$numberDecimal}</p>
-  //   </div>
-  // ));
+  const restaurantsCards = restaurants.map((restaurant) => (
+    <div
+      key={restaurant.title}
+      className="flex flex-col border-2 border-black gap-4"
+    >
+      {/* restaurant image */}
+      <p>{restaurant.title}</p>
+      <p>{restaurant.location[0]}</p> {/* Fixing the typo here */}
+    </div>
+  ));
 
   return (
-    <div className="">
+    <div className="flex">
       <header className="my-64">
-        <Image
+        {/* <Image
           src={foodImage}
           alt="food image"
           width={700}
           height={700}
           className="w-full h-[600px]"
-        />
+        /> */}
         <p className="text-8xl absolute top-[85%] text-white left-[7%]">
           JoDinner
         </p>
       </header>
       <div className="flex mx-32">
-        {dishesCards} {/* Render the dishes here */}
+        {/* Render the dishes here */}
+        {dishesCards}
       </div>
-      <div>{/* popular restaurants */}</div>
+      <hr />
+      <div className="flex mx-32">
+        {restaurantsCards}
+        {/* popular restaurants */}
+      </div>
       {/* footer */}
     </div>
   );
