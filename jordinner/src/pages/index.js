250cc8bebb6f2d02439d6927b399f12791155c6c
Rinad Abu Qauod <rinadaq98@gmail.com>
2024-03-21 16:53:19 +0300

    Added customer home page with design

--- jordinner/src/pages/index.js
+++ jordinner/src/pages/index.js
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import homepage1 from "/src/assets/homepage1.png";
 import Link from "next/link";
-// import homepage2 from "/src/assets/homepage2.png";
+import homepage2 from "/src/assets/homepage2.png";
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
 import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
+import { AiFillStar } from "react-icons/ai";
 
 export default function Home() {
   const [dishes, setDishes] = useState([]);
@@ -32,54 +33,73 @@ export default function Home() {
   }, []);
 
   const dishesCards = dishes.map((dish) => (
-    <div key={dish.name} className="dishInfo">
+    <Link href="/customer/dishes"><div key={dish.name} className="dishInfo">
       <Image className="dishImage" src={dish.image} alt="" objectFit="contain"></Image>
-      <p>{dish.name}</p>
-      <p>{dish.description}</p> {/* Fixing the typo here */}
-      <p>{dish.price.$numberDecimal}</p>
-      <Link href="/customer/signUpIn"><buttn className="addToCart"> <span>Add to cart</span></buttn></Link>
+      <p className="dishName">{dish.name}</p>
+      <p>{dish.description}</p>
+      <p>JOD {dish.price.$numberDecimal}</p>
     </div>
+    </Link>
   ));
 
   const restaurantsCards = restaurants.map((restaurant) => (
+    <Link href={`/customer/restaurants/${restaurant._id}`}>
     <div
-      key={restaurant.title}
-      className="restaurantInfo"
-    >
-      {/* restaurant image */} //we dont have images for restaurants though???
-      <p>{restaurant.title}</p>
-      <p>{restaurant.location[0]}</p> {/* Fixing the typo here */}
-      
+      key={restaurant.title} className="restaurantInfo">
+      <Image className="restaurantImg" src={restaurant.image} alt="" objectFit="contain"></Image>
+      <p className="restuarantTitle">{restaurant.title}</p>
+      <p>{restaurant.location[0]}</p>
+      <p className="rating"><AiFillStar className="ratingStar"/>{restaurant.rating}/5</p>
+      <p>{restaurant.category}</p>      
     </div>
+    </Link> 
+
   ));
+
+  function scrollSlider(sliderId, direction) {
+    const slider = document.getElementById(sliderId);
+    const scrollAmount = direction * 300;
+    slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    console.log(scrollAmount);
+  }
+  
 
   return (
     <section className="flex">
+      <div className="homepage-2">
+        <Image src={homepage2} alt="" layout={"fill"} objectFit="contain"></Image>
+      </div>
       <div className="welcomeContainer">
         <h1 className="welcomingHeader">Life tastes better with <span className="title">Jordinner!</span></h1>
-        <h2 className="welcomingText">Craving your next delicious Jordanaian adventure?</h2>
+        <h2 className="welcomingText">Craving your next delicious Jordanian adventure?</h2>
         <br></br>
         <Link href="/customer/signUpIn"><button className="orderNow">ORDER NOW!<IoArrowForwardCircleOutline className="orderArrow"/></button></Link>
-        <p className="partnerClick"><Link href="/restaurants//signup">Are you a restaurant?<IoArrowForwardCircleOutline className="restaurantArrow"/></Link></p>
-
+        <p className="partnerClick"><Link href="/restaurants/signup">Are you a restaurant?<IoArrowForwardCircleOutline className="restaurantArrow"/></Link></p>
       </div>
       <div className="homepage-1">
         <Image src={homepage1} alt="" layout={"fill"} objectFit="contain"></Image>
       </div>
-       {/* <div className="homepage-2">
-        <Image src={homepage2} alt="" layout={"fill"} objectFit="contain"></Image>
-      </div> */}
-      <div><h3 className="popularDishes">POPULAR DISHES</h3></div>
-      <div className="dishCards">
-        {/* Render the dishes here */}
-        <MdKeyboardArrowLeft className="dishArrow"/>
-        {dishesCards}
-        {/* <buttn className="addToCart"><span>Add to cart</span></buttn> */}<MdKeyboardArrowRight className="dishArrow"/>
-      </div>
-      <div><h3 className="popularRestaurants">POPULAR RESTAURANTS</h3></div>
+       
+     <div><h2 className="popularDishes"><Link href="/customer/dishes">EXPLORE DISHES</Link></h2></div>
+      <div className="dishCards" >
+         {/* Render dishes here */}
+           <MdKeyboardArrowLeft className="dishArrow" onClick={() => scrollSlider("dishSlider", -1)}/> 
+           <div className="CardsWrapper" id="dishSlider">
+          {dishesCards}
+        </div>
+          <MdKeyboardArrowRight className="dishArrow" onClick={() => scrollSlider("dishSlider", 1)}/> 
+        </div>
+      <div><h3 className="popularRestaurants"><Link href="/customer/restaurants">EXPLORE RESTAURANTS</Link></h3></div>
       <div className="restaurantCards">
-        {restaurantsCards}
-        {/* popular restaurants */}
+        <MdKeyboardArrowLeft className="dishArrow" onClick={() => scrollSlider("restaurantSlider", -1)}/>
+        <div className="CardsWrapper"  id="restaurantSlider">
+         
+          {restaurantsCards}
+          {/* renders restaurants */}
+          {/* clicking on the view button dircets user to single restuarant page // page not done yet*/}
+         
+        </div>
+        <MdKeyboardArrowRight className="dishArrow" onClick={() => scrollSlider("restaurantSlider", 1)}/>
       </div>
       {/* footer */}
     </section>
